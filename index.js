0a867b0867c07a185cd8b5bb482ddd57d26a64b2
Justin Jones <jus.jones@yahoo.com>
2022-05-18 21:14:29 -0400

    commit tree ad08f05816b50df0de8e6d11bbe74e71de682663

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', ()=>{alert('I was clicked!! by jay')})
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', ()=>{alert('I was clicked')})
+
+// const input = document.getElementById('input');
+
+// function clickAlert(){
+//   alert('I was clicked! jj')
+// }
+
+// input.addEventListener('click', clickAlert)
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+input.addEventListener('click', clickAlert);
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
